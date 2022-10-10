80caf3a2747740c65dff64dd1288035eb82f39cf
mariebot <72381163+mariebot@users.noreply.github.com>
2022-10-10 21:15:53 +0530

    Update play.py

--- RaiChu/Player/play.py
+++ RaiChu/Player/play.py
@@ -170,7 +170,7 @@ async def play(c: Client, m: Message):
                       reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("• Support", url=f"https://t.me/BotDuniyaXd"),
+                            InlineKeyboardButton("• Support", url=f"https://t.me/elisha_support"),
                             InlineKeyboardButton("• Close", callback_data="cls")
                         ]
                     ]
